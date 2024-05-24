97a291e2ce007327dc43a46c6d61932204d24daf
shmuel1997 <shmoeliy1997@gmail.com>
2024-05-24 11:47:40 +0300

    Connect angular application to firebase & firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,24 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideAnimations(),
     provideAnimationsAsync(),
+    provideFirebaseApp(() => 
+      initializeApp({
+        projectId: 'little-linguist2',
+        appId: '1:978986824361:web:f06e2fb11b0d8a8f8aafb8',
+        storageBucket: 'little-linguist2.appspot.com',
+        apiKey: 'AIzaSyA7YcNWgiFkUUkYBn5a0S6Ooj_A-MyzsHo',
+        authDomain: 'little-linguist2.firebaseapp.com',
+        messagingSenderId: '978986824361'
+      })
+    ),
+    provideFirestore(() => getFirestore()),
   ],
 };
